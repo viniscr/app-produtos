da2f22df8ebabfc8a49062293b13fca4feae1fe5
viniscr <vinicius.sa@ccc.ufcg.edu.br>
2017-11-15 15:54:46 -0300

    Projeto Completo

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 
 
-.controller('ProdutoController', function($scope,$ionicModal,$ionicPopup,$localStorage) {
+.controller('ProdutoController', function($scope,$ionicModal,$ionicPopup, $localStorage) {
   var produtosPadrao = [
     { titulo: 'Livro de Ionic', valor: 50.99, publicar:true },
     { titulo: 'Livro de Angular JS', valor: 40.99, publicar:true },
@@ -12,22 +12,16 @@ angular.module('starter.controllers', [])
 
   $scope.local = $localStorage;
 
-  //$scope.local.titulo = "Esse é o meu título";
-
   var init = function(){
-      if(!$scope.local.produtos){
-        $scope.local.produtos = produtosPadrao;
-        $scope.produtos = $scope.local.produtos;
-      }else{
-        $scope.produtos = $scope.local.produtos;
-      }
-      
-  };
+  	if(!$scope.local.produtos){
+  		$scope.local.produtos = produtosPadrao;
+  		$scope.produtos = $scope.local.produtos;
+  	}else{
+  		$scope.produtos = $scope.local.produtos;
+  	}
+  }
 
-
-
-
-  $ionicModal.fromTemplateUrl('templates/adicionar.html',{
+  $ionicModal.fromTemplateUrl('templates/adicionar.html', {
   	scope:$scope
   }).then(function(modal){
   	$scope.modal = modal;
@@ -42,17 +36,17 @@ angular.module('starter.controllers', [])
   }
 
   $scope.addProduto = function(Produto){
-    if(!validacao(Produto)){
-      return;
-    }
-  	$scope.produtos.push({titulo: Produto.titulo, valor: Produto.valor, publicar:Produto.publicar});
+  	if(!validacao(Produto)){
+  		return;
+  	}
+  	$scope.produtos.push({titulo: Produto.titulo, valor: Produto.valor, publicar: Produto.publicar});
   	Produto.titulo = "";
   	Produto.valor = "";
   	Produto.publicar = false;
   	$scope.modal.hide();
   }
 
-  $scope.Produto = {titulo:"",valor:"",publicar:false};
+  $scope.Produto = {titulo:"", valor:"", publicar: false};
   $scope.editando = false;
   var auxProdutoEditar;
 
@@ -63,59 +57,57 @@ angular.module('starter.controllers', [])
   	$scope.Produto.publicar = ProdutoEditar.publicar;
   	auxProdutoEditar = ProdutoEditar;
   	$scope.modal.show();
-  };
+  }
 
   $scope.salvarProduto = function(Produto){
-    if(!validacao(Produto)){
-      return;
-    }
+  	if(!validacao(Produto)){
+  		return;
+  	}
   	auxProdutoEditar.titulo = Produto.titulo;
   	auxProdutoEditar.valor = Produto.valor;
   	auxProdutoEditar.publicar = Produto.publicar;
   	$scope.modal.hide();
-  };
+  }
 
   $scope.deletarProduto = function(Produto){
-    for(var index in $scope.produtos){
-      var aux = $scope.produtos[index];
-      if(Produto === aux){
-        $scope.produtos.splice(index,1);
-      }
-    }
-  };
+  	for(var index in $scope.produtos){
+  		var aux = $scope.produtos[index];
+  		if(Produto === aux){
+  			$scope.produtos.splice(index, 1);
+  		}
+  	}
+  }
 
   $scope.deletando = false;
 
   var validacao = function(Produto){
 
-    var textoErro = "";
-    var existeErro = false;
+  	var textoErro = "";
+  	var existeErro = false;
 
-    if(Produto.titulo === ""){
-      existeErro = true;
-      textoErro += "<p>Preencha um título válido</p>";
-    }
+  	if(Produto.titulo === ""){
+  		existeErro = true;
+  		textoErro += "<p>Preencha um titulo válido</p>";
+  	}
 
-    if(Produto.valor === ""){
-      existeErro = true;
-      textoErro += "<p>Preencha um valor válido</p>";
-    }
+  	if(Produto.valor === ""){
+  		existeErro = true;
+  		textoErro += "<p>Preencha um valor válido</p>";
+  	}
 
-    if(existeErro){
-      var alertPopup = $ionicPopup.alert({
-        title:"Erro no Formulário",
-        template: textoErro
-      });
-      return false;
-    }else{
-      return true;
-    }
-
-    
-  };
+  	if(existeErro){
+	  	var alertPopup = $ionicPopup.alert({
+	  		title: "Erro no formulário",
+	  		template: textoErro
+	  	});
+	  	return false;
+  	}else{
+  		return true;
+  	}
+  
+  }
 
   init();
 
-
-})
+});
 
